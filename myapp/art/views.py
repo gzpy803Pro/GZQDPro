cbb33025bbef67745ee20a59dc45b3887a180941
disen <610039018@qq.com>
2018-09-21 16:32:57 +0800

    实现周排行

--- myapp/art/views.py
+++ myapp/art/views.py
@@ -2,11 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from art.models import BookModel
+from helper import week_rank
 
 
 # Create your views here.
 def show(request, id):
+
+    # 加入到周排行中
+    week_rank.add_rank(id)
+
     book = BookModel.objects.get(pk=id)
+
+    # 获取前5的周点击排行
+    rank_ids = week_rank.get_week_rank_ids(5)
+    rank_books = [BookModel.objects.get(pk=id_) for id_ in rank_ids]
+
     return render(request, 'book/show.html', locals())
 
     # return HttpResponse('<h1>show- &gt;'+book.name+'</h1><hr><img src="'+book.cover.url+'">',
---
+++ myapp/helper/__init__.py
@@ -0,0 +1,3 @@
+from redis import Redis
+
+rd_ = Redis(host='127.0.0.1', db=8)
